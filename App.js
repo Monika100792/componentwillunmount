7f1aceea826741c737564b47e02af0ec121d312d
Monika100792 <Monikadg4@gmail.com>
2024-11-05 12:08:06 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+class ComponentOne extends React.Component {
+  componentWillUnmount() {
+    alert('The component is going to be unmounted');
+  }
+
+  render() {
+    return <h1>Hello Geeks!</h1>;
+  }
+}
+
+class App extends React.Component {
+  state = { display: true };
+
+  delete = () => {
+    this.setState({ display: false });
+  };
+
+  render() {
+    let comp;
+    if (this.state.display) {
+      comp = <ComponentOne />;
+    }
+
+    return (
+      <div>
+        {comp}
+        <button onClick={this.delete}>Delete the component</button>
+      </div>
+    );
+  }
+}
+
+export default App;
